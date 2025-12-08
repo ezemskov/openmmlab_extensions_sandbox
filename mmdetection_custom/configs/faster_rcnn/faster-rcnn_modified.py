6258db2b58a9ebd82536f6d80f240ee4acaf470b
Eugene Zemskov <eugene.zemskow@shield.ai>
2025-12-08 16:18:56 +1100

    MovingAverageHook fixed

--- mmdetection_custom/configs/faster_rcnn/faster-rcnn_modified.py
+++ mmdetection_custom/configs/faster_rcnn/faster-rcnn_modified.py
@@ -11,7 +11,9 @@ custom_imports = dict(
 )
 
 custom_hooks = [
-    dict(type='MovingAvgLossHook', window_size=100, key='loss', out_key='loss_avg')
+    dict(type='MovingAvgLossHook', window_size=100, key='train/loss', out_key='train/loss_avg_100'),
+    dict(type='MovingAvgLossHook', window_size=1000, key='train/loss', out_key='train/loss_avg_1K'),
+    dict(type='MovingAvgLossHook', window_size=100, key='train/acc', out_key='train/acc_avg_100'),
 ]
 
 visualizer_config = dict(
--- mmdetection_custom/hooks/moving_avg_hook.py
+++ mmdetection_custom/hooks/moving_avg_hook.py
@@ -9,21 +9,19 @@ class MovingAvgLossHook(Hook):
     Logs a moving average of the training loss so ClearML can display it.
     """
 
-    def __init__(self, window_size=100, key='loss', out_key='loss_avg'):
+    def __init__(self, key, out_key, window_size=100):
         self.window_size = window_size
         self.key = key
         self.out_key = out_key
         self.buffer = deque(maxlen=window_size)
 
     def after_train_iter(self, runner, batch_idx, data_batch=None, outputs=None):
-        # Latest logged values for the "train" group
-        log_vars = runner.message_hub.log_scalars()
+        valBuf = runner.message_hub.get_scalar(self.key)
+        currVal = float(valBuf.current())
+        self.buffer.append(currVal)
+        avg_val = sum(self.buffer) / len(self.buffer)
 
-        if self.key in log_vars:
-            val = float(log_vars[self.key])
-            self.buffer.append(val)
-
-            avg_val = sum(self.buffer) / len(self.buffer)
-
-            # Send smoothed loss metric into MMEngine's scalar system
-            runner.message_hub.update_scalar(self.out_key, avg_val)
+        # Send smoothed loss metric into MMEngine's scalar system
+        runner.message_hub.update_scalar(self.out_key, avg_val)
+"""
+"""        
